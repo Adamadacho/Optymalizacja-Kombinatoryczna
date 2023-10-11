c49c618f9aa9645cf9d9a0750232a8d7a1f49ac2
Adamadacho <adamkublinski@gmail.com>
2023-10-11 08:41:24 +0200

    Update ZADANIE_1.py
    OK

--- ZADANIE_1.py
+++ ZADANIE_1.py
@@ -1,7 +1,7 @@
 import itertools
 import argparse
 
-# Funkcja do obliczania odległości między dwoma punktami na płaszczyźnie euklidesowe
+# Funkcja do obliczania odległości między dwoma punktami na płaszczyźnie euklidesowejj
 def distance(p1, p2):
     return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
 
