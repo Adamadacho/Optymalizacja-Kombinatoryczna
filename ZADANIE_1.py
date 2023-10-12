a17fcbd3f4b28ac761c6ce91616f40e67b7acaf2
Adamadacho <adamkublinski@gmail.com>
2023-10-12 20:38:17 +0200

    ok

--- ZADANIE_1.py
+++ ZADANIE_1.py
@@ -1,43 +1,37 @@
 import itertools
 import argparse
 
-# Funkcja do obliczania odległości między dwoma punktami na płaszczyźnie euklidesowejj
 def distance(p1, p2):
     return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
 
-# Funkcja do obliczania sumy odległości między punktami w danym podzbiorze
-def calculate_total_distance(points, subset):
-    total_distance = 0
+def calcTotalDistance(points, subset):
+    totalDistance = 0
     for i in range(len(subset)):
         for j in range(i+1, len(subset)):
-            total_distance += distance(points[subset[i]], points[subset[j]])
-    return total_distance
+            totalDistance += distance(points[subset[i]], points[subset[j]])
+    return totalDistance
 
-# Parsowanie argumentów wiersza poleceń
+
 parser = argparse.ArgumentParser(description='Rozwiązywanie problemu wyboru k-elementowego podzbioru punktów na płaszczyźnie euklidesowej.')
 parser.add_argument('-k', type=int, required=True, help='Liczba elementów w podzbiorze')
 parser.add_argument('-f', type=str, required=True, help='Ścieżka do pliku z danymi wejściowymi')
 args = parser.parse_args()
 
-# Wczytywanie punktów z pliku
-points = [] # Deklaracja pustej listy
-with open(args.f, 'r') as file: #Otwiera z kontekstem plik -f 
+points = [] 
+with open(args.f, 'r') as file: 
     for line in file:
         x, y = map(int, line.strip().split(','))
         points.append((x, y))
 
-# Parametry problemu
 k = args.k
-max_total_distance = 0
-best_subset = []
+maxTotalDistance = 0
+bestSubset = []
 
-# Iterujemy przez wszystkie możliwe kombinacje k-elementowe
 for subset in itertools.combinations(range(len(points)), k):
-    total_distance = calculate_total_distance(points, subset)
-    if total_distance > max_total_distance:
-        max_total_distance = total_distance
-        best_subset = subset
+    totalDistance = calcTotalDistance(points, subset)
+    if totalDistance > maxTotalDistance:
+        maxTotalDistance = totalDistance
+        bestSubset = subset
 
-# Wyprowadzanie wyników
-print(round(max_total_distance, 2))  # Zaokrąglamy wynik do dwóch miejsc po przecinku
-print(sorted(best_subset))
+print(round(maxTotalDistance, 2))  
+print(sorted(bestSubset))
--- prog.py
+++ prog.py
@@ -1,20 +1,37 @@
-import math
 import itertools
 import argparse
 
 def distance(p1, p2):
-    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+    return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
 
-def calculateTotalDistance(points, subset):
+def calcTotalDistance(points, subset):
     totalDistance = 0
     for i in range(len(subset)):
         for j in range(i+1, len(subset)):
-            totalDistance += distance(points(subset[i])), distance(points(subset[j]))
+            totalDistance += distance(points[subset[i]], points[subset[j]])
     return totalDistance
-        
-parser = argparse.ArgPar()
-parser.add_argument('-k', type=int, required=True, help='Liczba elementów w podzbiorze Q')
+
+
+parser = argparse.ArgumentParser(description='Rozwiązywanie problemu wyboru k-elementowego podzbioru punktów na płaszczyźnie euklidesowej.')
+parser.add_argument('-k', type=int, required=True, help='Liczba elementów w podzbiorze')
 parser.add_argument('-f', type=str, required=True, help='Ścieżka do pliku z danymi wejściowymi')
 args = parser.parse_args()
 
+points = [] 
+with open(args.f, 'r') as file: 
+    for line in file:
+        x, y = map(int, line.strip().split(','))
+        points.append((x, y))
 
+k = args.k
+maxTotalDistance = 0
+bestSubset = []
+
+for subset in itertools.combinations(range(len(points)), k):
+    totalDistance = calcTotalDistance(points, subset)
+    if totalDistance > maxTotalDistance:
+        maxTotalDistance = totalDistance
+        bestSubset = subset
+
+print(round(maxTotalDistance, 2))  
+print(sorted(bestSubset))
